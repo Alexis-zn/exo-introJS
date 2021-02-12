e4dd369817b8194395cee95bd1cf57de83bf2045
Alexis-zn <alexis.zentici@gmail.com>
2021-02-12 15:21:30 +0100

    Done

--- prez.js
+++ prez.js
@@ -2,5 +2,5 @@ let prenom ='Sofiane';
 let age = 39;
 let ville ='Paris';
 
-console.log('Hello, my name is '+ prenom +' and i am '+ age +' years old. I live in '+ ville +'.');
+console.log(`Hello, my name is ${prenom} and i am ${age} years old. I live in ${ville}.`);
 
